311ce7fa06dac8d4fa7e8fe0b4b4222f9f518ada
yangsl <yangshunlicn@163.com>
2023-11-15 21:37:20 +0800

    第二次提交

---
+++ account-springIoc/src/main/java/cn/ysl/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.ysl.git;
+
+/**
+ * Description:GitTest
+ *
+ * @Author Yang ShunLi
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
