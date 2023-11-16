d2c2b00e1dc80b3ee92d377b706fa9ac189c696c
yangshunlicn <40462613+yangshunlicn@users.noreply.github.com>
2023-11-16 16:30:43 +0800

    Update GitTest.java

--- account-springIoc/src/main/java/cn/ysl/git/GitTest.java
+++ account-springIoc/src/main/java/cn/ysl/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello master");
         System.out.println("hello hotfix");
+        System.out.println("hello github repo");
     }
 }
