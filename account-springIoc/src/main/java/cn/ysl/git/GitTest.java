df3f9500a268c630810b85ca173e2e6fc8e0eb6d
yangsl <yangshunlicn@163.com>
2023-11-16 17:19:48 +0800

    master 更新1

--- account-springIoc/src/main/java/cn/ysl/git/GitTest.java
+++ account-springIoc/src/main/java/cn/ysl/git/GitTest.java
@@ -15,7 +15,6 @@ public class GitTest {
         System.out.println("hello github repo2");
         System.out.println("hello github");
         System.out.println("hello idea");
-
         System.out.println("pull hello git");
     }
 }
